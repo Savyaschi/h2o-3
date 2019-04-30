0def0c4d16e2cb38fa3b46e88a42a07685d04a3e
michalkurka <michalk@h2o.ai>
2019-04-30 16:37:02 -0700

    almost removed all newchunks..

--- h2o-algos/src/main/java/hex/svm/PrimalDualIPM.java
+++ h2o-algos/src/main/java/hex/svm/PrimalDualIPM.java
@@ -1,10 +1,7 @@
 package hex.svm;
 
 import water.MRTask;
-import water.fvec.Chunk;
-import water.fvec.Frame;
-import water.fvec.NewChunk;
-import water.fvec.Vec;
+import water.fvec.*;
 import water.util.ArrayUtils;
 import water.util.Log;
 
@@ -15,41 +12,50 @@ public class PrimalDualIPM {
 
     final double c_pos = params._weight_positive * params._hyper_parm;
     final double c_neg = params._weight_negative * params._hyper_parm;
-    final long num_constraints = rbicf.numRows() * 2; 
+    final long num_constraints = rbicf.numRows() * 2;
 
-    Vec x = label.makeZero();
-    Frame initFrame = new InitTask(c_pos, c_neg).doAll(new byte[]{Vec.T_NUM, Vec.T_NUM}, label).outputFrame();
-    Vec la = initFrame.vec(0);
-    Vec xi = initFrame.vec(1);
+    Frame workspace = new Frame(new String[]{"label"}, new Vec[]{label});
+    workspace.add("x", label.makeZero());
+    workspace.add(new String[]{"z", "xi", "dxi", "la", "dla", "tlx", "tux", "xilx", "laux", "d", "dx"}, label.makeVolatileDoubles(11));
+
+    new InitTask(c_pos, c_neg).doAll(workspace);
+
+    Vec z = workspace.vec("z");
+    Vec la = workspace.vec("la");
+    Vec xi = workspace.vec("xi");
+
+    Vec x = workspace.vec("x");
+    Vec dxi = workspace.vec("dxi");
+    Vec dla = workspace.vec("dla");
+    Vec d = workspace.vec("d");
 
     double nu = 0;
     boolean converged = false;
     for (int iter = 0; iter < params._max_iter; iter++) {
-      double eta = computeSurrogateGap(c_pos, c_neg, label, x, la, xi);
-      double t = (params._mu_factor * num_constraints) / eta;
+      final double eta = new SurrogateGapTask(c_pos, c_neg).doAll(workspace)._sum;
+      final double t = (params._mu_factor * num_constraints) / eta;
       Log.debug("sgap: " + eta + " t: " + t);
 
-      Vec z = computePartialZ(rbicf, x, params._tradeoff);
-      CheckConvergenceTask cct = new CheckConvergenceTask(nu).doAll(z, label, la, x, xi);
+      computePartialZ(rbicf, x, params._tradeoff, z);
+      CheckConvergenceTask cct = new CheckConvergenceTask(nu).doAll(workspace);
       converged = cct._resp <= params._feasible_threshold && cct._resd <= params._feasible_threshold && eta <= params._sgap_bound;
       if (converged) {
         break;
       }
 
-      Frame working = new UpdateVarsTask(c_pos, c_neg, params._x_epsilon, t)
-              .doAll(new byte[]{Vec.T_NUM, Vec.T_NUM, Vec.T_NUM, Vec.T_NUM, Vec.T_NUM}, z, label, la, x, xi)
-              .outputFrame(new String[]{"tlx", "tux", "xilx", "laux", "d"}, null); 
+      new UpdateVarsTask(c_pos, c_neg, params._x_epsilon, t).doAll(workspace);
 
-      LLMatrix icfA = MatrixUtils.productMM(rbicf, working.vec("d"));
+      LLMatrix icfA = MatrixUtils.productMM(rbicf, d);
       LLMatrix lra = MatrixUtils.cf(icfA);
 
-      final double dnu = computeDeltaNu(rbicf, working.vec("d"), label, z, x, lra);
-      Vec dx = computeDeltaX(rbicf, working.vec("d"), label, dnu, lra, z);
+      final double dnu = computeDeltaNu(rbicf, d, label, z, x, lra);
+      Vec dx = computeDeltaX(rbicf, d, label, dnu, lra, z);
 
-      LineSearchTask lst = new LineSearchTask(c_pos, c_neg).doAll(new byte[]{Vec.T_NUM, Vec.T_NUM}, label, working.vec("tlx"), working.vec("tux"), working.vec("xilx"), working.vec("laux"), xi, la, dx, x);
-      Frame lstFrame = lst.outputFrame(new String[]{"dxi", "dla"}, null);
+      workspace.replace(workspace.find("dx"), dx);
+      
+      LineSearchTask lst = new LineSearchTask(c_pos, c_neg).doAll(workspace);
 
-      new MakeStepTask(lst._ap, lst._ad).doAll(x, dx, xi, lstFrame.vec("dxi"), la, lstFrame.vec("dla"));
+      new MakeStepTask(lst._ap, lst._ad).doAll(x, dx, xi, dxi, la, dla);
 
       nu += lst._ad * dnu;
     }
@@ -60,6 +66,33 @@ public class PrimalDualIPM {
     }
 
     return x;
+  }
+
+  private static abstract class PDIPMTask<E extends PDIPMTask<E>> extends MRTask<E> {
+    Chunk _label, _x;
+    Chunk _z;
+    Chunk _xi, _dxi, _la, _dla;
+    Chunk _tlx, _tux, _xilx, _laux, _d;
+    Chunk _dx;
+    @Override
+    public void map(Chunk[] cs) {
+      _label = cs[0];
+      _x = cs[1];
+      _z = cs[2];
+      _xi = cs[3];
+      _dxi = cs[4];
+      _la = cs[5];
+      _dla = cs[6];
+      _tlx = cs[7];
+      _tux = cs[8];
+      _xilx = cs[9];
+      _laux = cs[10];
+      _d = cs[11];
+      _dx = cs[12];
+      map();
+    }
+
+    abstract void map();
   }
 
   static class MakeStepTask extends MRTask<MakeStepTask> {
@@ -86,7 +119,7 @@ public class PrimalDualIPM {
     
   }
 
-  static class LineSearchTask extends MRTask<LineSearchTask> {
+  static class LineSearchTask extends PDIPMTask<LineSearchTask> {
     private final double _c_pos;
     private final double _c_neg;
 
@@ -98,18 +131,15 @@ public class PrimalDualIPM {
       _c_neg = c_neg;
     }
 
-    public void map(Chunk[] cs, NewChunk[] ncs) {
-      map(cs[0], cs[1], cs[2], cs[3], cs[4], cs[5], cs[6], cs[7], cs[8], ncs[0], ncs[1]);
+    @Override
+    public void map() {
+      map(_label, _tlx, _tux, _xilx, _laux, _xi, _la, _dx, _x, ((C8DVolatileChunk) _dxi).getValues(), ((C8DVolatileChunk) _dla).getValues());
     }
 
-    private void map(Chunk label, Chunk tlx, Chunk tux, Chunk xilx, Chunk laux, Chunk xi, Chunk la, Chunk dx, Chunk x, NewChunk dxiC, NewChunk dlaC) {
-      double[] dxi = new double[tlx._len];
-      double[] dla = new double[tlx._len];
+    private void map(Chunk label, Chunk tlx, Chunk tux, Chunk xilx, Chunk laux, Chunk xi, Chunk la, Chunk dx, Chunk x, double[] dxi, double[] dla) {
       for (int i = 0; i < dxi.length; ++i) {
         dxi[i] = tlx.atd(i) - xilx.atd(i) * dx.atd(i) - xi.atd(i);
-        dxiC.addNum(dxi[i]);
         dla[i] = tux.atd(i) + laux.atd(i) * dx.atd(i) - la.atd(i);
-        dlaC.addNum(dla[i]);
       }
       double ap = Double.MAX_VALUE;
       double ad = Double.MAX_VALUE;
@@ -150,7 +180,7 @@ public class PrimalDualIPM {
       throw new IllegalArgumentException("Expected a binary response encoded as +1/-1");
   }
   
-  static class UpdateVarsTask extends MRTask<UpdateVarsTask> {
+  static class UpdateVarsTask extends PDIPMTask<UpdateVarsTask> {
     private final double _c_pos;
     private final double _c_neg;
     private final double _epsilon_x;
@@ -162,35 +192,30 @@ public class PrimalDualIPM {
       _epsilon_x = epsilon_x;
       _t = t;
     }
-
-    @Override
-    public void map(Chunk[] cs, NewChunk[] ncs) {
-      map(cs[0], cs[1], cs[2], cs[3], cs[4], ncs[0], ncs[1], ncs[2], ncs[3], ncs[4]);
-    }
     
-    private void map(Chunk z, Chunk label, Chunk la, Chunk x, Chunk xi,
-                     NewChunk tlx, NewChunk tux, NewChunk xilx, NewChunk laux, NewChunk d) {
-      for (int i = 0; i < z._len; i++) {
-        double c = (label.atd(i) > 0) ?_c_pos : _c_neg;
-        double m_lx = Math.max(x.atd(i), _epsilon_x);
-        double m_ux = Math.max(c - x.atd(i), _epsilon_x);
+    @Override
+    void map() {
+      for (int i = 0; i < _z._len; i++) {
+        double c = (_label.atd(i) > 0) ?_c_pos : _c_neg;
+        double m_lx = Math.max(_x.atd(i), _epsilon_x);
+        double m_ux = Math.max(c - _x.atd(i), _epsilon_x);
         double tlxi = 1.0 / (_t * m_lx);
         double tuxi = 1.0 / (_t * m_ux);
-        tlx.addNum(tlxi);
-        tux.addNum(tuxi);
+        _tlx.set(i, tlxi);
+        _tux.set(i, tuxi);
         
-        double xilxi = Math.max(xi.atd(i) / m_lx, _epsilon_x);
-        double lauxi = Math.max(la.atd(i) / m_ux, _epsilon_x);
-        d.addNum(1.0 / (xilxi + lauxi));
-        xilx.addNum(xilxi);
-        laux.addNum(lauxi);
+        double xilxi = Math.max(_xi.atd(i) / m_lx, _epsilon_x);
+        double lauxi = Math.max(_la.atd(i) / m_ux, _epsilon_x);
+        _d.set(i, 1.0 / (xilxi + lauxi));
+        _xilx.set(i, xilxi);
+        _laux.set(i, lauxi);
         
-        z.set(i, tlxi - tuxi - z.atd(i));
+        _z.set(i, tlxi - tuxi - _z.atd(i));
       }
     }
   }
   
-  static class CheckConvergenceTask extends MRTask<CheckConvergenceTask> {
+  static class CheckConvergenceTask extends PDIPMTask<CheckConvergenceTask> {
     private final double _nu;
     // OUT
     double _resd;
@@ -201,18 +226,14 @@ public class PrimalDualIPM {
     }
 
     @Override
-    public void map(Chunk[] cs) {
-      map(cs[0], cs[1], cs[2], cs[3], cs[4]);
-    }
-
-    public void map(Chunk z, Chunk label, Chunk la, Chunk x, Chunk xi) {
-      for (int i = 0; i < z._len; i++) {
-        double zi = z.atd(i);
-        zi += _nu * (label.atd(i) > 0 ? 1 : -1) - 1.0;
-        double temp = la.atd(i) - xi.atd(i) + zi;
-        z.set(i, zi);
+    void map() {
+      for (int i = 0; i < _z._len; i++) {
+        double zi = _z.atd(i);
+        zi += _nu * (_label.atd(i) > 0 ? 1 : -1) - 1.0;
+        double temp = _la.atd(i) - _xi.atd(i) + zi;
+        _z.set(i, zi);
         _resd += temp * temp;
-        _resp += label.atd(i) * x.atd(i);
+        _resp += _label.atd(i) * _x.atd(i);
       }
     }
 
@@ -229,23 +250,24 @@ public class PrimalDualIPM {
     }
   }
 
-  private static Vec computePartialZ(Frame rbicf, Vec x, final double tradeoff) {
+  private static void computePartialZ(Frame rbicf, Vec x, final double tradeoff, Vec z) {
     final Vec[] vecs = ArrayUtils.append(rbicf.vecs(), x);
     final double vz[] = new MatrixMultVecTask().doAll(vecs)._row;
-    return new MRTask() {
+    new MRTask() {
       @Override
-      public void map(Chunk[] cs, NewChunk z) {
-        final int p = cs.length - 1;
+      public void map(Chunk[] cs) {
+        final int p = cs.length - 2;
         final Chunk x = cs[p];
+        final Chunk z = cs[p + 1];
         for (int i = 0; i < cs[0]._len; i++) {
           double s = 0;
           for (int j = 0; j < p; j++) {
             s += cs[j].atd(i) * vz[j];
           }
-          z.addNum(s - tradeoff * x.atd(i));
+          z.set(i, s - tradeoff * x.atd(i));
         }
       }
-    }.doAll(Vec.T_NUM, vecs).outputFrame().anyVec();
+    }.doAll(ArrayUtils.append(vecs, z));
   }
   
   static class MatrixMultVecTask extends MRTask<MatrixMultVecTask> {
@@ -271,11 +293,7 @@ public class PrimalDualIPM {
     }
   }
 
-  private static double computeSurrogateGap(double c_pos, double c_neg, Vec response, Vec x, Vec la, Vec xi) {
-    return new SurrogateGapTask(c_pos, c_neg).doAll(new Vec[]{response, x, la, xi})._sum;
-  }
-
-  static class SurrogateGapTask extends MRTask<SurrogateGapTask> {
+  static class SurrogateGapTask extends PDIPMTask<SurrogateGapTask> {
     private final double _c_pos;
     private final double _c_neg;
     
@@ -286,22 +304,18 @@ public class PrimalDualIPM {
       _c_pos = c_pos;
       _c_neg = c_neg;
     }
-
+    
     @Override
-    public void map(Chunk[] cs) {
-      _sum = map(cs[0], cs[1], cs[2], cs[3]);
-    }
-
-    public double map(Chunk label, Chunk x, Chunk la, Chunk xi) {
+    void map() {
       double s = 0;
-      for (int i = 0; i < x._len; i++) {
-        double c = (label.atd(i) > 0.0) ? _c_pos : _c_neg;
-        s += la.atd(i) * c;
+      for (int i = 0; i < _x._len; i++) {
+        double c = (_label.atd(i) > 0.0) ? _c_pos : _c_neg;
+        s += _la.atd(i) * c;
       }
-      for (int i = 0; i < x._len; i++) {
-        s += x.atd(i) * (xi.atd(i) - la.atd(i));
+      for (int i = 0; i < _x._len; i++) {
+        s += _x.atd(i) * (_xi.atd(i) - _la.atd(i));
       }
-      return s;
+      _sum = s;
     }
 
     @Override
@@ -310,7 +324,7 @@ public class PrimalDualIPM {
     }
   }
   
-  static class InitTask extends MRTask<InitTask> {
+  static class InitTask extends PDIPMTask<InitTask> {
     private final double _c_pos;
     private final double _c_neg;
 
@@ -320,12 +334,11 @@ public class PrimalDualIPM {
     }
 
     @Override
-    public void map(Chunk[] cs, NewChunk[] nc) {
-      Chunk label = cs[0];
-      for (int i = 0; i < label._len; i++) {
-        double c = ((label.atd(i) > 0) ? _c_pos : _c_neg) / 10;
-        nc[0].addNum(c);
-        nc[1].addNum(c);
+    public void map() {
+      for (int i = 0; i < _label._len; i++) {
+        double c = ((_label.atd(i) > 0) ? _c_pos : _c_neg) / 10;
+        _la.set(i, c);
+        _xi.set(i, c);
       }
     }
   }
